ab6946db40806379dcecd3e9217d5a1dc4633fc1
Joe <J_rdavid24@outlook.com>
2026-02-20 21:00:30 -0600

    workflow status

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { PORT = 3001 } = process.env;
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
-    console.log("Connected to DB");
+    console.log(" we are connected to DB");
   })
   .catch(console.error);
 
