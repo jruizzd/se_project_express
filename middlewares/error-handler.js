c193dab95d8c87ac48f51d798361ff6d8851f39a
Joe <J_rdavid24@outlook.com>
2026-02-21 09:31:10 -0600

    updated errorHandler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,13 +1,12 @@
 const errorHandler = (err, req, res, next) => {
-  // Log the error for debugging
   console.error(err);
 
-  // Check if it's a custom error with a status code
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "An error occurred on the server";
+  const { statusCode = 500, message } = err;
 
-  // Send the response
-  res.status(statusCode).send({ message });
+  res.status(statusCode).send({
+    message:
+      statusCode === 500 ? "An error has occurred on the server" : message,
+  });
 };
 
 module.exports = errorHandler;
